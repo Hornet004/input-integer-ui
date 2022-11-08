5a8405e8d64a1516adf82637ee9442e167242848
Hornet004 <abelawonusi@gmail.com>
2022-11-08 20:23:05 +0100

    edited demo.js adding title and subtitle and also using queryselector

--- demo/demo.js
+++ demo/demo.js
@@ -1,6 +1,24 @@
 const input_interger = require('..')
 
-const inputInteger = input_interger()
+const input1 = input_interger()
+const input2 = input_interger()
+
+const title = `My demo form`
+const subTitle = `Please feel out the form` 
+
+const page = document.createElement('div')
+page.innerHTML =`
+<h1> ${title}</h1>
+<h2> ${subTitle}</h1>
+<x></x>
+<h3> Enter year of birth</h3>
+<y></y>
+
+`
+page.querySelector('x').replaceWith(input1)
+page.querySelector('y').replaceWith(input2)
 
 
-document.body.append(inputInteger)
+
+
+document.body.append(page)
